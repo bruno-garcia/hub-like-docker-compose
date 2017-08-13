78f8107f0a7169c45b6af148d7e29e9eb0b46e48
Bruno Garcia <bruno@brunogarcia.com>
2017-08-14 00:37:23 +0200

    api connects to redis on docker compose

---
+++ api/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace Api
+{
+    public class Program
+    {
+        public static IConfigurationRoot Configuration { get; private set; }
+
+        public static void Main(string[] args)
+        {
+            Configuration = new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .AddCommandLine(args)
+                .Build();
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls("http://*:5000")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
---
+++ api/Startup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
+
+namespace Api
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IApplicationLifetime applicationLifetime)
+        {
+            var logger = loggerFactory
+                .AddConsole(LogLevel.Debug)
+                .CreateLogger<Startup>();
+
+            var redisConfiguration = Program.Configuration["Redis"];
+            if (redisConfiguration == null) throw new ArgumentException("No Redis configuration provided.");
+
+            if (env.IsDevelopment())
+            {
+                logger.LogInformation($"Connecting to: Redis: {redisConfiguration}");
+                app.UseDeveloperExceptionPage();
+            }
+
+            var redis = ConnectionMultiplexer.Connect(redisConfiguration);
+            applicationLifetime.ApplicationStopping.Register(() => redis.Dispose());
+
+            app.Run(async (context) =>
+            {
+                if (context.Request.Path.Value == "/favicon.ico")
+                    return;
+
+                var counter = await redis.GetDatabase(0).StringIncrementAsync("ApiHitCounter");
+                await context.Response.WriteAsync($"Redis hit counter: {counter}");
+            });
+        }
+    }
+}
