686133fadce2d9b76c0e08748c08c2bf8fde1d48
Bruno Garcia <bruno@brunogarcia.com>
2017-08-13 12:04:59 +0200

    envoy connects to mssql on docker-compose

---
+++ envoy/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace Envoy
+{
+    public class Program
+    {
+        public static IConfigurationRoot Configuration { get; private set; }
+        
+        public static void Main(string[] args)
+        {
+            Configuration = new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .AddCommandLine(args)
+                .Build();
+
+            var host = new WebHostBuilder()
+                .UseConfiguration(Configuration)
+                .UseKestrel()
+                .UseUrls("http://*:5000")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
---
+++ envoy/Startup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Envoy
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            var logger = loggerFactory
+                .AddConsole(LogLevel.Debug)
+                .CreateLogger<Startup>();
+            
+            var connectionString = Program.Configuration.GetConnectionString("DefaultConnection");
+            if (connectionString == null) throw new ArgumentException("No default connection string provided.");
+                
+            if (env.IsDevelopment())
+            {
+                logger.LogInformation($"Connection string: {connectionString}");
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                using (var connection = new SqlConnection(connectionString))
+                using (var command = new SqlCommand("SELECT 1", connection))
+                {
+                    await connection.OpenAsync();
+                    var result = await command.ExecuteScalarAsync();
+                    await context.Response.WriteAsync($"SELECT 1 magically returned: {result}");
+                }                
+            });
+        }
+    }
+}
